c156d549f7ad3edfda2473fdc6e7901e5c31e34a
Bllopp <pablo.ratouit@efrei.net>
2023-11-08 15:22:33 +0100

    yarn dependencies and add and sub
    Co-authored-by: mustache956 <mustache956@users.noreply.github.com>
    Co-authored-by: Senhao JIANG <senhao.jiang@efrei.net>
    Co-authored-by: CrosmosEgg <CrosmosEgg@users.noreply.github.com>

--- src/answer.js
+++ src/answer.js
@@ -1,3 +1,48 @@
 const getAnswer = () => 42;
 
-module.exports = getAnswer;
+
+var stack = [];
+
+const getStack = () => {
+    tokens.forEach(t => {
+        if(typeof t === 'string' && !Number.isNaN(t)) {
+            stack.push(t)
+        }
+    });
+    return stack;
+};
+
+const rpn = (string) => {
+    const tokens = string.split(' ')
+    const operators = ["+", "-"]
+
+    
+    tokens.forEach(t => {
+        if(typeof t === 'string' && !Number.isNaN(t)) {
+            stack.push(t)
+        }
+        var res;
+        switch (t) {
+            case "+":
+                stack.push(stack.pop() + stack.pop());
+                break;
+            
+            case "-":
+                res = -stack.pop() + stack.pop();
+                break;
+                
+        
+            default:
+                new Error("invalid entry");
+                break;
+        }
+        
+    });
+
+    return stack;
+}
+
+
+module.exports = {getAnswer, getStack, rpn};
+
+// 20 2 +
--- src/answer.test.js
+++ src/answer.test.js
@@ -1,8 +1,30 @@
-const getAnswer = require('./answer')
+const {getAnswer, getStack, add, rpn} = require('./answer')
 
 describe('getting answers', () => {
   test('it returns 42', () => {
     const res = getAnswer()
     expect(res).toBe(42)
+  })
+})
+
+describe('get stack', () => {
+  test('get stack', () => {
+    rpn("1 1 +");
+    const res = getStack();
+    expect(res).toBe([1,1])
+  })
+})
+
+describe('add number', () => {
+  test('1 + 1 = 2', () => {
+    const res = rpn("");
+    expect(res).toBe(2)
+  })
+})
+
+describe('soustract number', () => {
+  test('3 + 2 = 1', () => {
+    const res = rpn("3 2 -");
+    expect(res).toBe(1)
   })
 })
